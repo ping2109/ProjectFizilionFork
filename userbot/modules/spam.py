5d7a5c637d306dceea88a5ef8d91ef4b8f7bac89
AbOuLfOoOoOuF <67458252+AbOuLfOoOoOuF@users.noreply.github.com>
2021-08-29 21:19:49 +0300

    spam: fixed spam by reply and errors
    spam by reply was limited to text only, now it works for any message.
    Fixed if no input was given or it's wrong, the bot would send an error.

--- userbot/modules/spam.py
+++ userbot/modules/spam.py
@@ -47,11 +47,20 @@ async def word_spam(wspammer):
 async def spammer(spamm):
     try:
         counter = int(spamm.pattern_match.group(1).split(" ", 1)[0])
-    except ValueError:
+    except IndexError:
+        await spamm.edit("The usage of this command is .spam <count> <text> or .spam <count> replyiing to a message")
+        await sleep(5)
+        await spamm.delete()
         return
     textx = await spamm.get_reply_message()
     if not textx:
-        spam_message = str(spamm.pattern_match.group(1).split(" ", 1)[1])
+        try:
+            spam_message = str(spamm.pattern_match.group(1).split(" ", 1)[1])
+        except IndexError:
+            await spamm.edit("The usage of this command is .spam <count> <text> or .spam <count> replyiing to a message")
+            await sleep(5)
+            await spamm.delete()
+            return
         await spamm.delete()
         await asyncio.wait([spamm.respond(spam_message) for i in range(counter)])
         if BOTLOG:
@@ -86,8 +95,14 @@ async def drepeattt(event):
 async def tiny_pic_spam(pspam):
     message = pspam.text
     text = message.split()
-    counter = int(text[1])
-    link = str(text[2])
+    try:
+        counter = int(text[1])
+        link = str(text[2])
+    except IndexError:
+        await pspam.edit("The usage of this command is .picspam <count> <link to image/gif>")
+        await sleep(5)
+        await pspam.delete()
+        return
     await pspam.delete()
     for _ in range(1, counter):
         await pspam.client.send_file(pspam.chat_id, link)
@@ -99,9 +114,15 @@ async def tiny_pic_spam(pspam):
 
 @register(outgoing=True, pattern="^\{trg}delayspam (.*)".format(trg=trgg))
 async def dspammer(dspam):
-    spamDelay = float(dspam.pattern_match.group(1).split(" ", 2)[0])
-    counter = int(dspam.pattern_match.group(1).split(" ", 2)[1])
-    spam_message = str(dspam.pattern_match.group(1).split(" ", 2)[2])
+    try:
+        spamDelay = float(dspam.pattern_match.group(1).split(" ", 2)[0])
+        counter = int(dspam.pattern_match.group(1).split(" ", 2)[1])
+        spam_message = str(dspam.pattern_match.group(1).split(" ", 2)[2])
+    except IndexError:
+        await dspam.edit("The usage of this command is .delayspam <delay> <count> <text>")
+        await sleep(5)
+        await dspam.delete()
+        return
     await dspam.delete()
     for _ in range(1, counter):
         await dspam.respond(spam_message)
